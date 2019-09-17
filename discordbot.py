7069c9d27cf451ebdd1c516dd7336020ab0ec5c8
akuaQuiet <55394741+akuaQuiet@users.noreply.github.com>
2019-09-18 02:32:15 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -16,15 +16,7 @@ async def on_ready():
 
 
 
-# メッセージ受信時に動作する処理
-@client.event
-async def on_message(message):
-    # メッセージ送信者がBotだった場合は無視する
-    if message.author.bot:
-        return
-    # 「/neko」と発言したら「にゃーん」が返る処理
-    if message.content == '/neko':
-        await message.channel.send('くーちゃんきゃわわ')
+
 
 
 @client.event
