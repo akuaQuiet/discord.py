2d7522b1fd60a21cbcb58cd66074f80f148d71a6
akuaQuiet <55394741+akuaQuiet@users.noreply.github.com>
2019-10-08 07:11:13 +0900

    Update python discordbot.py

--- python discordbot.py
+++ python discordbot.py
@@ -2,7 +2,7 @@
 import discord
 
 # 自分のBotのアクセストークンに置き換えてください
-TOKEN = 'NjIzMTQ4ODM5MTA4NjA4MDI4.XZu2Lg.vXkWd0o6x3WuqpD7ROwczS8SiXc'
+TOKEN = 'NjIzMTQ4ODM5MTA4NjA4MDI4.XZu36w.NBwGX2d938DTLZkHMKBFzWBsOeY'
 
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
